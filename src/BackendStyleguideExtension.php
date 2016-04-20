71c58827d7a167e671fa84eb614595390fabde48
Bob den Otter <bobdenotter@gmail.com>
2016-04-20 16:35:07 +0200

    Adding Styleguide template.

--- src/BackendStyleguideExtension.php
+++ src/BackendStyleguideExtension.php
@@ -3,6 +3,8 @@
 namespace Bolt\Extension\Bobdenotter\BackendStyleguide;
 
 use Bolt\Extension\SimpleExtension;
+use Bolt\Menu\MenuEntry;
+use Silex\ControllerCollection;
 
 /**
  * Backen Styleguide Extension class.
@@ -13,5 +15,32 @@ class BackendStyleguideExtension extends SimpleExtension
 {
 
 
+    protected function registerMenuEntries()
+    {
+        $menu = new MenuEntry('backend-styleguide', 'styleguide');
+        $menu->setLabel('Backend Styleguide')
+            ->setIcon('fa:diamond')
+            ->setPermission('settings')
+        ;
+
+        return [
+            $menu,
+        ];
+    }
+
+
+    protected function registerBackendRoutes(ControllerCollection $collection)
+    {
+        $collection->get('/extend/styleguide', [$this, 'callbackStyleguide']);
+    }
+
+    public function callbackStyleguide()
+    {
+        $context = [
+            'name' => 'Kenny Koala',
+            'home' => 'Gum Tree Lane',
+        ];
+        return $this->renderTemplate('backend-styleguide.twig', $context);
+    }
 
 }
